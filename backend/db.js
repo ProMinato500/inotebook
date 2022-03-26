a1b8d44d93f09f91843ff90a7ebc3dac697da94a
ProMinato500 <nandip500@gmail.com>
2022-03-26 23:01:11 +0530

    added db connection and models

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const mongoose = require('mongoose');
 
-const connectToMongo = () => {
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected");
-    })
+const mongoURI = "mongodb://0.0.0.0:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+    const connectToMongo = ()=>{
+        mongoose.connect(mongoURI).then(() => {
+            console.log("Connected to MongoDB successfully :)");
+        }).catch((e) => {
+            console.log("Error while attempting to connect to MongoDB");
+            console.log(e);
+        });
 }
+
+
+
 
 module.exports = connectToMongo;
