8f10af6b1daab94f0ec29f78a4d8d558320e7d73
ProMinato500 <nandip500@gmail.com>
2022-03-30 00:37:50 +0530

    added notes routes

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
+
+//Schema for notes containing it's components and their types
 const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user' //will refer to a particular user
+    },
     title:{
         type: String,
         required: true
@@ -11,11 +18,11 @@ const NotesSchema = new Schema({
     },
     tag:{
         type: String,
-        default: "General"
+        default: "General" //by default will show general as tag
     },
     date:{
         type: Date,
-        default: Date.now
+        default: Date.now  //will show the current date as date
     }
 })
 
