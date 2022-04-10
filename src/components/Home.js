7207e06df253a765cc2835adcd1e62746e10768f
ProMinato500 <nandip500@gmail.com>
2022-04-11 02:58:23 +0530

    added cards and alert

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
-import React, { useContext } from 'react';
-import noteContext from '../context/notes/NoteContext';
+import Notes from "./Notes";
 
 export const Home = () => {
-  const context = useContext(noteContext)
-  const {notes, setNotes} = context
+
   return (
     <div>
       <div className="container my-3">
@@ -48,12 +46,7 @@ export const Home = () => {
           </button>
         </form>
         </div>
-      <div className="container">
-        <h1>Your notes</h1>
-        {notes.map((note) => {
-          return note.title
-        })}
-      </div>
+      <Notes/>
     </div>
   );
 }
